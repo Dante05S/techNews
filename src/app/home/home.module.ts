446e9692906a2324d2f284bb81bbb671c7bbd97a
Dante05S <Dante05S@hotmail.com>
2021-09-02 16:18:44 -0500

    initial commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { Banner1Component } from "./banner1/banner1.component";
+import { HomePageRoutingModule } from './home-routing.module';
+import { NewsComponent } from "./news/news.component";
+import { Banner2Component } from "./banner2/banner2.component";
+import { Banner3Component } from "./banner3/banner3.component";
+import { RedesComponent } from "./redes/redes.component";
+import { FooterComponent } from "./footer/footer.component";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [
+    HomePage,
+    NavBarComponent,
+    Banner1Component,
+    NewsComponent,
+    Banner2Component,
+    Banner3Component,
+    RedesComponent,
+    FooterComponent
+  ]
+})
+export class HomePageModule { }
