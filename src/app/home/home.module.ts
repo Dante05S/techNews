c8a0b27d62ab77b46f121a28a14c7a4d0ff21089
Dante05S <Dante05S@hotmail.com>
2021-09-02 23:53:44 -0500

    added ionic to project

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,13 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { Banner1Component } from "./banner1/banner1.component";
+import { NewsComponent } from "./news/news.component";
+import { Banner2Component } from "./banner2/banner2.component";
+import { Banner3Component } from "./banner3/banner3.component";
+import { RedesComponent } from "./redes/redes.component";
+import { FooterComponent } from "./footer/footer.component";
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -14,6 +20,14 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,
+    NavBarComponent,
+    Banner1Component,
+    NewsComponent,
+    Banner2Component,
+    Banner3Component,
+    RedesComponent,
+    FooterComponent
+  ]
 })
 export class HomePageModule {}
